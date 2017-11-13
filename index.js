107e0ac108e3dc52737f11ac97f9fe476ab6369a
Thakur Ballary <thakurballary@gmail.com>
2017-11-13 17:57:41 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+var plans = new Vue({
+	el: '#plans',
+	data: {
+		plans: [
+			{
+				name: "30 Pack",
+				cost_per_can: "60 INR per can",
+				total_cost: "INR 1,800"
+			},
+			{
+				name: "60 Pack",
+				cost_per_can: "54 INR per can",
+				total_cost: "INR 3,240"
+			},
+			{
+				name: "90 Pack",
+				cost_per_can: "54 INR per can",
+				total_cost: "INR 4,860"
+			}
+		]
+	}
+});
